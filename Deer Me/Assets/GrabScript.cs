c1997e737c15128555118437e4b787f159cec97c
mbruts101 <bruts101@mail.chapman.edu>
2017-10-22 20:50:52 -0700

    +1

--- Deer Me/Assets/GrabScript.cs
+++ Deer Me/Assets/GrabScript.cs
@@ -6,6 +6,7 @@ public class GrabScript : MonoBehaviour {
     public Transform player;
     private Rigidbody2D rBody;
     private bool grabbed;
+    public GameObject grabObj;
 	// Use this for initialization
 	void Start () {
         grabbed = false;
@@ -13,33 +14,38 @@ public class GrabScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        
-	}
+      
+    }
     void OnTriggerStay2D(Collider2D col)
     {
         if(col.gameObject.tag == "Grabbable")
         {
-            if (Input.GetButtonDown("Grab") && !grabbed)
+            if (Input.GetButtonDown("Grab") && grabbed)
             {
-                col.transform.parent = this.transform;
-             //   rBody = col.GetComponent<Rigidbody2D>();
-            //    rBody.isKinematic = false;
+                Debug.Log("trying to drop");
+                grabObj.transform.parent = null;
+                rBody = grabObj.GetComponent<Rigidbody2D>();
+                rBody.isKinematic = false;
+                grabbed = false;
+            }
+            else if (Input.GetButtonDown("Grab") && !grabbed)
+            {
+                grabObj = col.gameObject;
+                grabObj.transform.parent = this.transform;
+                rBody = grabObj.GetComponent<Rigidbody2D>();
+                rBody.isKinematic = true;
                 grabbed = true;
             }
+
             if (grabbed)
             {
                 if (Input.GetKey(KeyCode.D))
                 {
-                    col.transform.Translate(Vector3.right * 7 * Time.deltaTime);
+                    //col.transform.Translate(Vector3.right * 7 * Time.deltaTime);
                 }
             }
-            if (Input.GetButtonDown("Grab") && !grabbed)
-            {
-                col.transform.parent = null;
-             //   rBody = col.GetComponent<Rigidbody2D>();
-             //   rBody.isKinematic = true;
-                grabbed = false;
-            }
+     
+
         }
     }
 }
