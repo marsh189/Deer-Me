b78531714e3e60fca5ff4f415cd0a430890a77ec
mbruts101 <bruts101@mail.chapman.edu>
2017-10-22 20:49:59 -0700

    Many Changes, read description
    Added camera follow script, changed character controller to a new and
    improved one.  Working triggers and no collision issues. Functioning
    grab and drop.

---
+++ Deer Me/Assets/CameraScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    private Vector2 velocity;
+
+    public float smoothY;
+    public float smoothX;
+
+    public GameObject Player;
+    
+    void Start ()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+    void FixedUpdate()
+    {
+        float posX = Mathf.SmoothDamp(transform.position.x, Player.transform.position.x, ref velocity.x, smoothX);
+        float posY = Mathf.SmoothDamp(transform.position.y, Player.transform.position.y, ref velocity.x, smoothY);
+
+        transform.position = new Vector3(posX, posY, transform.position.z);
+    }
+}
