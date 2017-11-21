6e1f6f3868e48dc16e5b0a2d37efd79aced78c15
mbruts101 <bruts101@mail.chapman.edu>
2017-11-21 14:03:46 -0800

    Added lever mechanics, cleaned up files.

---
+++ Deer Me/Assets/MyAssets/Scripts/LeverScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverScript : MonoBehaviour {
+    public GameObject gameObjectReference;
+    public bool leverActive = false;
+    public GameObject NewPositionObj;
+    public GameObject OldPositionObj;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!leverActive && OldPositionObj.transform.position != gameObjectReference.transform.position)
+        {
+            gameObjectReference.transform.position = Vector3.MoveTowards(gameObjectReference.transform.position, OldPositionObj.transform.position, Time.deltaTime * 2);
+        }
+        
+        if (leverActive && NewPositionObj.transform.position != gameObjectReference.transform.position)
+        {
+            gameObjectReference.transform.position = Vector3.MoveTowards(gameObjectReference.transform.position, NewPositionObj.transform.position, Time.deltaTime * 2);          
+        }
+	}
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Debug.Log("Colliding");
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+                Debug.Log("pressing x");
+                if (leverActive && OldPositionObj.transform.position != gameObjectReference.transform.position)
+                {
+                    Debug.Log("moving down");
+                    leverActive = false;
+                }
+                else if(!leverActive && NewPositionObj.transform.position != gameObjectReference.transform.position)
+                {
+                    Debug.Log("moving up");
+                    leverActive = true;
+                }
+               
+            }
+        }
+    }
+}
