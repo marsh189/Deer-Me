0a997cc69571536eba0c615f1f207b2a9d91a28f
mbruts101 <bruts101@mail.chapman.edu>
2017-11-25 14:40:54 -0800

    Added Pause Menu, GameManager, UIManager
    Readded Killzones

---
+++ Deer Me/Assets/MyAssets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//https://vilbeyli.github.io/Unity3D-How-to-Make-a-Pause-Menu/
+public class GameManager : MonoBehaviour {
+    public UIManager UI;
+	// Use this for initialization
+	void Start () {
+        UI.GetComponentInChildren<Canvas>().enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void TogglePauseMenu()
+    {
+        if (UI.GetComponentInChildren<Canvas>().enabled)
+        {
+            UI.GetComponentInChildren<Canvas>().enabled = false;
+            Time.timeScale = 1.0f;
+        }
+        else
+        {
+            UI.GetComponentInChildren<Canvas>().enabled = true;
+            Time.timeScale = 0f;
+        }
+    }
+}
---
+++ Deer Me/Assets/MyAssets/Scripts/UIManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+    public GameManager gm;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            gm.TogglePauseMenu();
+        }
+	}
+
+    public void Resume()
+    {
+        gm.TogglePauseMenu();
+    }
+    public void Quit()
+    {
+        //Application.quit?? Or maybe go to a menu
+    }
+}
