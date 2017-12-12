4dbbf45952a4339f6637d87c0a78466bb446dbdd
mbruts101 <bruts101@mail.chapman.edu>
2017-12-11 23:15:25 -0800

    adsafsd

---
+++ Deer Me/Assets/PushingScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushingScript : MonoBehaviour {
+    public GameObject Player;
+    public Animator anim;
+	// Use this for initialization
+	void Start () {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        anim = Player.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            anim.SetBool("Pushing", true);
+        }
+       
+    }
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            anim.SetBool("Pushing", false);
+        }
+    }
+}
