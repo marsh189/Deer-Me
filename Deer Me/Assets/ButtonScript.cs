0be3500b8e9fb9ea716355a0264bd0081443f706
mbruts101 <bruts101@mail.chapman.edu>
2018-02-28 09:54:36 -0800

    More Mill, fixed backgrounds

---
+++ Deer Me/Assets/ButtonScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour {
+    public GameObject gameObjectReference;
+    public bool buttonActive = false;
+    public GameObject NewPositionObj;
+    public GameObject OldPositionObj;
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!buttonActive && OldPositionObj.transform.position != gameObjectReference.transform.position)
+        {
+            gameObjectReference.transform.position = Vector3.MoveTowards(gameObjectReference.transform.position, OldPositionObj.transform.position, Time.deltaTime * 2);
+        }
+
+        if (buttonActive && NewPositionObj.transform.position != gameObjectReference.transform.position)
+        {
+            gameObjectReference.transform.position = Vector3.MoveTowards(gameObjectReference.transform.position, NewPositionObj.transform.position, Time.deltaTime * 2);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Box")
+        {
+            buttonActive = true;
+        }
+    }
+}
