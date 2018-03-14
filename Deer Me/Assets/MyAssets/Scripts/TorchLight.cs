5b8eca8efa3377c213fce60407ef4e3dcdb24e77
Vince Carpino <carpi111@mail.chapman.edu>
2018-03-14 13:39:24 -0700

    Fix most of light leaks in ground pieces

---
+++ Deer Me/Assets/MyAssets/Scripts/TorchLight.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class TorchLight : MonoBehaviour {
+
+	public float minRand;
+	public float maxRand;
+	public float updateFrequency;
+	public float brightnessChangeValue;
+
+	private Light thisLight;
+	private Color originalColor;
+	private float timePassed;
+	private float changeValue;
+
+	private float randValue;
+
+	void Start () {
+		thisLight = GetComponent<Light>();
+
+		if (thisLight != null) {
+			originalColor = thisLight.color;
+		} else {
+			enabled = false;
+			return;
+		}
+
+		changeValue = 0;
+		timePassed = 0;
+
+		InvokeRepeating("ChangeTorchFrequency", 0f, updateFrequency);
+	}
+
+	void Update () {
+		timePassed = Time.time;
+		timePassed = timePassed - Mathf.Floor(timePassed);
+
+		thisLight.color = originalColor * CalculateChange();
+
+//		StartCoroutine(ChangeTorchFrequency(1f));
+	}
+
+	private float CalculateChange() {
+		changeValue = -Mathf.Sin(timePassed * randValue * Mathf.PI) * brightnessChangeValue + 0.95f;
+		return changeValue;
+	}
+
+	void ChangeTorchFrequency() {
+//		yield return new WaitForSeconds(val);
+
+		randValue = Random.Range(minRand, maxRand);
+	}
+}
