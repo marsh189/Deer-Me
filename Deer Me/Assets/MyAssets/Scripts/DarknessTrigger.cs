343ba46b689cea68a0abf312f4e6cc502b324ba7
Vince Carpino <carpi111@mail.chapman.edu>
2018-04-09 00:57:16 -0700

    Still tweaking darkness section; incomplete

--- Deer Me/Assets/MyAssets/Scripts/DarknessTrigger.cs
+++ Deer Me/Assets/MyAssets/Scripts/DarknessTrigger.cs
@@ -7,50 +7,65 @@
  *
  * Controls darkness stage of level.
  *     As player advances forward,
- *     the screen darkens. If they
- *     turn around, the screen
- *     lightens again.
+ *     world darkens; lightens when
+ *     they move backwards.
  */
 
+using System.Collections;
+using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class DarknessTrigger : MonoBehaviour {
 
-	public float startXPos;
+//	public float startXPos;
 
 	public float divisor;
-	public float minVisibility;
+//	public float minVisibility;
 
 	public GameObject player;
-	public Image darknessImage;
+//	public Image darknessImage;
 
 	public Light worldLight;
 
 	public bool hasTorch;
 	private bool hitTrigger;
 
+	private float currPos, prevPos;
+
 	private void Start() {
-		minVisibility /= 100f;
-		minVisibility = 1f - minVisibility;
+//		minVisibility /= 100f;
+//		minVisibility = 1f - minVisibility;
 	}
 
 	void Update() {
-//		CheckPosition();
 
 		if (hitTrigger) {
-			printPos();
+            CheckPosition();
+			StartCoroutine(WaitForTime());
+//			printPos();
 		}
 	}
 
 	void CheckPosition() {
-		if (player.transform.position.x <= startXPos) return;
+		// FIGURE OUT BETTER WAY TO TRACK PLAYER MOVEMENT TO AFFECT WORLD LIGHT INTENSITY
+		currPos = player.transform.position.x;
 
-		var tempColor = darknessImage.color;
-		tempColor.a = (player.transform.position.x - startXPos) / divisor;
-		if (tempColor.a <= minVisibility) {
-			darknessImage.color = tempColor;
-		}
+//		if (player.transform.position.x <= startXPos) return;
+
+//		var tempColor = darknessImage.color;
+//		tempColor.a = (player.transform.position.x - startXPos) / divisor;
+//		if (tempColor.a <= minVisibility) {
+//			darknessImage.color = tempColor;
+//		}
+
+		var playerDelta = player.transform.position.x - gameObject.transform.position.x;
+//		print(playerDelta);
+
+		print(currPos - prevPos);
+		worldLight.intensity -= (currPos - prevPos) / divisor;
+
+		prevPos = currPos;
 	}
 
 	void printPos() {
@@ -65,10 +80,14 @@ public class DarknessTrigger : MonoBehaviour {
 		print("TRIGGER DARK");
 
 		// CHANGE BRIGHTNESS OF WORLD LIGHT AS PLAYER MOVES FORWARD
-		worldLight.intensity = 0.5f;
+//		worldLight.intensity = 0.5f;
 	}
 
 	public void setHasTorch(bool val) {
 		hasTorch = val;
+	}
+
+	IEnumerator WaitForTime() {
+		yield return new WaitForSeconds(0.2f);
 	}
 }
