89962af329e83f1ff88d5b2fd5221d13ce299c8c
mbruts101 <bruts101@mail.chapman.edu>
2018-04-09 09:40:17 -0700

    TextBox
    You can now read books

---
+++ Deer Me/Assets/TextBoxManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBoxManager : MonoBehaviour {
+    public TextAsset file;
+    public string[] line;
+    public Text TextBoxText;
+    public GameManager textBoxObj;
+    public int curr = 0;
+    public int fileLength;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setText(TextAsset filename)
+    {
+        file = filename;
+        curr = 0;
+        if (file != null)
+        {
+            line = file.text.Split('\n');
+        }
+        fileLength = line.Length - 1;
+    }
+    public void readText()
+    {
+        textBoxObj.enabled = true;
+        while(curr < fileLength)
+        {
+            TextBoxText.text = line[curr];
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                curr += 1;
+            }
+        }
+        textBoxObj.enabled = false;
+    }
+}
---
+++ Deer Me/Assets/TextSetter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextSetter : MonoBehaviour {
+    public TextAsset file;
+    public TextBoxManager tbm;
+	// Use this for initialization
+	void Start () {
+        tbm = GameObject.FindGameObjectWithTag("TextBoxManager").GetComponent<TextBoxManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            tbm.setText(file);
+            if (Input.GetKeyDown(KeyCode.E)){
+                tbm.readText();
+            }
+        }
+    }
+}
