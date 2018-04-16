1f44a3a1bcb38332da135e5d5ba45e8c4fc6a72e
mbruts101 <bruts101@mail.chapman.edu>
2018-04-16 16:47:56 -0700

    Bugs plus start of book anim

---
+++ Deer Me/Assets/MyAssets/Scripts/TextBoxManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBoxManager : MonoBehaviour {
+    public TextAsset file;
+    public string[] line;
+    public Text TextBoxText;
+    public GameObject textBoxObj;
+    public int curr = 0;
+    public int fileLength;
+    public bool canRead;
+    public bool finishedReading;
+    public Animator boxAnim;
+    // Use this for initialization
+    void Start()
+    {
+        canRead = false;
+        boxAnim = textBoxObj.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(canRead == true)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if(curr > fileLength)
+                {
+            
+                    finishedReading = true;
+                    //Time.timeScale = 1.0f;
+                    boxAnim.SetBool("Reading", false);
+                    textBoxObj.SetActive(false);
+                    curr = 0;
+                }
+                else if(curr <= fileLength)
+                {
+                    if(curr == 0)
+                    {
+                        Debug.Log("Animation");
+                        boxAnim.SetBool("Reading", true);
+                    }
+                    finishedReading = false;
+                    //Time.timeScale = 0f;
+                    textBoxObj.SetActive(true);
+                    Debug.Log("TextActive");
+                    Debug.Log("Line: " + curr);
+                    TextBoxText.text = line[curr];
+                    curr += 1;
+                }
+                
+            }
+        }
+    }
+
+    public void setText(TextAsset filename)
+    {
+        file = filename;
+        curr = 0;
+        if (file != null)
+        {
+            line = file.text.Split('\n');
+        }
+        fileLength = line.Length - 1;
+    }   
+}
