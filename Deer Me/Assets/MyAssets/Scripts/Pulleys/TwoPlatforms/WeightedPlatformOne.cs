1a41a1f8c76dcd75b41e68caf0fc806270a813d3
marsh189 <marsh189@mail.chapman.edu>
2018-05-02 17:27:20 -0700

    First attempt on weighted pulley

---
+++ Deer Me/Assets/MyAssets/Scripts/Pulleys/TwoPlatforms/WeightedPlatformOne.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeightedPlatformOne : MonoBehaviour {
+
+    public float massNeeded;
+    public float currMass;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player" || col.gameObject.tag == "Box")
+        {
+            currMass += col.gameObject.GetComponent<Rigidbody2D>().mass;
+
+            if (currMass == massNeeded)
+            {
+                GetComponentInParent<PlatformPulley>().onPlatform_1 = true;
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player" || col.gameObject.tag == "Box")
+        {
+            currMass -= col.gameObject.GetComponent<Rigidbody2D>().mass;
+            if (currMass != massNeeded)
+            {
+                GetComponentInParent<PlatformPulley>().onPlatform_1 = false;
+            }
+        }
+    }
+}
